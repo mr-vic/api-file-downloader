2f124586d8d2f783f4102f6b5e7dbe532112d99e
Viktor Tkachenko <mr.vic@bk.ru>
2022-10-26 19:10:19 +0500

    spread code

--- download.py
+++ download.py
@@ -1,8 +1,12 @@
-from asyncio.windows_events import NULL
-from contextlib import nullcontext
 import requests
 
-def download(endpoint, data, dirPath, token = None):
-    headers = {"Authorization": "Bearer {token}"}
-    header = requests.get(endpoint, data=data, headers=headers)
-    return header
+def saveToFile(dirPath, content, fileName):
+    with open(fileName, "wb") as f:
+        f.write(content)
+
+def download(endpoint, dirPath, fileName, token = None):
+    headers = {"Authorization": f'Bearer {token}'}
+    res = requests.get(endpoint, headers=headers)
+    contentType = res.headers['content-type']
+    saveToFile(dirPath, res.content, fileName)
+    return res
--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 from fileinput import filename
-import requests
+#import requests
 import os
 from dotenv import load_dotenv
-#import download
+from download import download
 
 load_dotenv()
 getName = os.environ["getName"]
 getParam = os.environ["getParam"]
 url = os.environ["url"]
 authToken= os.environ["authToken"]
-
-def saveToFile(dirPath, content, fileName):
-    with open(fileName, "wb") as f:
-        f.write(content)
-
-def download(endpoint, dirPath, fileName, token = None):
-    headers = {"Authorization": f'Bearer {token}'}
-    res = requests.get(endpoint, headers=headers)
-    contentType = res.headers['content-type']
-    saveToFile(dirPath, res.content, fileName)
-    return res
 
 if __name__ == '__main__':
     print(download(f'{url}{getName}={getParam}', "C:\\backup", "response.jpg", token=authToken))
