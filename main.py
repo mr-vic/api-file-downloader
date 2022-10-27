f265176266f68f400c4e069078c17f8f71eccd24
Viktor Tkachenko <mr.vic@bk.ru>
2022-10-27 12:57:52 +0500

    save/load id from txt file

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 from fileinput import filename
 import os
 from dotenv import load_dotenv
-from tools import download
+from tools import download, saveToFile, loadFromFile
 import psycopg
 
 load_dotenv()
-getName = os.environ["getName"]
-getParam = os.environ["getParam"]
 url = os.environ["url"]
 dirPath = os.environ["dirPath"]
 authToken= os.environ["authToken"]
 connString = os.environ["connString"]
-
+logFile = "log.txt"
 
 if __name__ == '__main__':
-
+    id = loadFromFile(logFile)
     with psycopg.connect(connString) as conn:
         conn.execute("SET client_encoding TO UTF8")
         with conn.cursor() as cur:
-            id = 2
-            cur.execute("SELECT * FROM \"Files\" WHERE \"Files\".\"Id\"=%s;", (id,))
-            row = cur.fetchone()
-            filename = row[1]
-            print(filename)
-
-
-    
-    #print(download(f'{url}{getName}={getParam}', dirPath, "response.jpg", token=authToken))
+            # get last id from table select max(id) from table
+            cur.execute("SELECT MAX(\"Files\".\"Id\") FROM \"Files\"")
+            lastId = int(cur.fetchone()[0])
+            print(lastId)
+            for i in range(id, lastId):              
+                cur.execute("SELECT * FROM \"Files\" WHERE \"Files\".\"Id\"=%s;", (id,))
+                row = cur.fetchone()
+                filename = row[1]
+                print(filename)
+                # download file
+                download(f'{url}{getName}={getParam}', dirPath, "response.jpg", token=authToken)
+                saveToFile("", str(id), logFile, "txt")
--- tools.py
+++ tools.py
@@ -1,14 +1,22 @@
 import requests
 
-def saveToFile(dirPath, content, fileName):
-    print(dirPath + fileName)
-    with open(dirPath + fileName, "wb") as f:
-        f.write(content)
+def saveToFile(dirPath, content, fileName, type="bin"):
+    if type == "bin":
+        with open(dirPath + fileName, "wb") as f:
+            f.write(content)
+    elif type == "txt":
+        with open(dirPath + fileName, "w") as f:
+            f.write(content)
 
 def download(endpoint, dirPath, fileName, token = None):
     headers = {"Authorization": f'Bearer {token}'}
     res = requests.get(endpoint, headers=headers)
     contentType = res.headers['content-type']
-    saveToFile(dirPath, res.content, fileName)
+    saveToFile(dirPath, res.content, fileName, "bin")
     return res
 
+def loadFromFile(fileName):
+    result = ""
+    with open(fileName) as f:
+        result = f.readline()
+    return result
