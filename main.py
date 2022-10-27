0b43246265a0b6b4842f00be44b4ad958cac76f7
Viktor Tkachenko <mr.vic@bk.ru>
2022-10-27 14:02:38 +0500

    work app

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fileinput import filename
 import os
+from types import NoneType
 from dotenv import load_dotenv
 from tools import download, saveToFile, loadFromFile
 import psycopg
@@ -12,7 +13,7 @@ connString = os.environ["connString"]
 logFile = "log.txt"
 
 if __name__ == '__main__':
-    id = loadFromFile(logFile)
+    id = int(loadFromFile(logFile))
     with psycopg.connect(connString) as conn:
         conn.execute("SET client_encoding TO UTF8")
         with conn.cursor() as cur:
@@ -20,11 +21,16 @@ if __name__ == '__main__':
             cur.execute("SELECT MAX(\"Files\".\"Id\") FROM \"Files\"")
             lastId = int(cur.fetchone()[0])
             print(lastId)
-            for i in range(id, lastId):              
+            for i in range(id, lastId + 1):              
                 cur.execute("SELECT * FROM \"Files\" WHERE \"Files\".\"Id\"=%s;", (id,))
                 row = cur.fetchone()
-                filename = row[1]
-                print(filename)
-                # download file
-                download(f'{url}{getName}={getParam}', dirPath, "response.jpg", token=authToken)
-                saveToFile("", str(id), logFile, "txt")
+                if(row != None):
+                    #debug info
+                    print(row)
+                    filename = row[1]
+                    print(id, filename)
+                    # download file
+                    download(f'{url}{id}', dirPath, filename, token=authToken)
+                    # save current id value into txt file
+                    saveToFile("", str(id), logFile, "txt")
+                id += 1
